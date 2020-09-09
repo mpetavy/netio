9d00e04ceb5c1eecf36915223e4ec85fc77e3e08
mpetavy <mpetavy>
2020-09-09 23:04:19 +0200

    tty support

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/jacobsa/go-serial/serial"
 	"github.com/mpetavy/common"
 	"hash"
 	"io"
 	"io/ioutil"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -31,11 +33,11 @@ var (
 	loopTimeout         *int
 	hashAlg             *string
 	randomBytes         *bool
-	interTimeout        *int
+	loopSleep           *int
 )
 
 func init() {
-	common.Init(false, "1.0.0", "2019", "network performance testing tool", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, run, 0)
+	common.Init(false, "1.0.0", "2019", "network/serial performance testing tool", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, run, 0)
 
 	client = flag.String("c", "", "client socket address or TTY port")
 	server = flag.String("s", "", "server socket address")
@@ -50,7 +52,7 @@ func init() {
 	writeThrottleString = flag.String("wt", "0", "write throttled bytes/sec")
 	loopCount = flag.Int("lc", 10, "loop count")
 	loopTimeout = flag.Int("lt", common.DurationToMillisecond(time.Second), "loop timeout")
-	interTimeout = flag.Int("it", 0, "intermediate transfer test loop")
+	loopSleep = flag.Int("ls", 0, "loop sleep timeout between loop steps")
 }
 
 func startSession() hash.Hash {
@@ -71,15 +73,102 @@ func startSession() hash.Hash {
 	return hasher
 }
 
-func endSession(socket net.Conn, hasher hash.Hash) {
+func endSession(socket io.ReadWriteCloser, hasher hash.Hash) {
 	if socket != nil {
-		common.Info("Disconnect: %s", socket.RemoteAddr().String())
+		if asSocket(socket) != nil {
+			common.Info("Disconnect: %s", asSocket(socket).RemoteAddr().String())
+		} else {
+			common.Info("Disconnect: %s", *client)
+		}
+
 		if hasher != nil {
 			common.Info("%s: %x", strings.ToUpper(*hashAlg), hasher.Sum(nil))
 		}
 
 		common.Ignore(socket.Close())
 	}
+}
+
+func isSerialPortOptions(txt string) bool {
+	return strings.Contains(txt, ",") || !strings.Contains(txt, ":")
+}
+
+func asSocket(rwc io.ReadWriteCloser) net.Conn {
+	socket, ok := rwc.(net.Conn)
+
+	if !ok {
+		return nil
+	}
+
+	return socket
+}
+
+func evaluateSerialPortOptions(txt string) (*serial.OpenOptions, error) {
+	ss := strings.Split(txt, ",")
+
+	baudrate := 9600
+	databits := 8
+	stopbits := 1
+	paritymode := serial.PARITY_NONE
+	pm := "N"
+
+	var portname string
+	var err error
+
+	portname = ss[0]
+	if len(ss) > 1 {
+		baudrate, err = strconv.Atoi(ss[1])
+		if err != nil {
+			return nil, fmt.Errorf("invalid baudrate: %s", ss[1])
+		}
+	}
+	if len(ss) > 2 {
+		databits, err = strconv.Atoi(ss[2])
+		if err != nil {
+			return nil, fmt.Errorf("invalid databits: %s", ss[2])
+		}
+	}
+	if len(ss) > 3 {
+		pm = strings.ToUpper(ss[3][:1])
+
+		switch pm {
+		case "N":
+			paritymode = serial.PARITY_NONE
+		case "O":
+			paritymode = serial.PARITY_ODD
+		case "E":
+			paritymode = serial.PARITY_EVEN
+		default:
+			return nil, fmt.Errorf("invalid partitymode: %s", pm)
+		}
+	}
+
+	if len(ss) > 4 {
+		stopbits, err = strconv.Atoi(ss[4])
+		if err != nil {
+			return nil, fmt.Errorf("invalid stopbits: %s", ss[3])
+		}
+	}
+
+	common.Info("Serial Port: %s Baudrate: %d Databits: %d ParityMode: %s Stopbits: %d", portname, baudrate, databits, pm, stopbits)
+
+	return &serial.OpenOptions{
+		PortName:          portname,
+		BaudRate:          uint(baudrate),
+		DataBits:          uint(databits),
+		StopBits:          uint(stopbits),
+		ParityMode:        paritymode,
+		RTSCTSFlowControl: false,
+
+		InterCharacterTimeout:   0,
+		MinimumReadSize:         1,
+		Rs485Enable:             false,
+		Rs485RtsHighDuringSend:  false,
+		Rs485RtsHighAfterSend:   false,
+		Rs485RxDuringTx:         false,
+		Rs485DelayRtsBeforeSend: 0,
+		Rs485DelayRtsAfterSend:  0,
+	}, nil
 }
 
 func run() error {
@@ -98,12 +187,12 @@ func run() error {
 		return err
 	}
 
-	var socket net.Conn
+	var socket io.ReadWriteCloser
 	var tlsSocket *tls.Conn
 	var tcpListener *net.TCPListener
 	var listener net.Listener
 
-	if *server != "" {
+	if *server != "" && !isSerialPortOptions(*server) {
 		if *useTls {
 			tlsPackage, err := common.GetTlsPackage()
 			if common.Error(err) {
@@ -137,51 +226,83 @@ func run() error {
 		common.Info("WRITE throttle bytes/sec: %s = %d Bytes", *writeThrottleString, writeThrottle)
 
 		if *server != "" {
-			ips, err := common.GetHostAddrs(true, nil)
-			if common.Error(err) {
-				return err
-			}
+			if isSerialPortOptions(*server) {
+				var spo *serial.OpenOptions
 
-			common.Info("Local IPs: %v", ips)
-			common.Info("Accept connection: %s...", *server)
+				spo, err = evaluateSerialPortOptions(*server)
+				if common.Error(err) {
+					return err
+				}
 
-			socketCh := make(chan net.Conn)
-			socket = nil
+				socket, err = serial.Open(*spo)
+				if common.Error(err) {
+					return err
+				}
 
-			go func() {
+				var err error
+				var n int
+				var a int
 
-				if *useTls {
-					s, err := listener.Accept()
-					common.Error(err)
-
-					tlsConn, ok := s.(*tls.Conn)
-					if ok {
-						err := tlsConn.Handshake()
-
-						if common.Error(err) {
-							common.Error(s.Close())
-						}
+				buf := make([]byte, blockSize)
+				for {
+					n, err = socket.Read(buf)
+					if common.Error(err) {
+						return err
 					}
 
-					if s != nil {
-						socketCh <- s
-					}
-				} else {
-					s, err := tcpListener.AcceptTCP()
-					common.Error(err)
+					if n > 0 {
+						a += n
 
-					if s != nil {
-						socketCh <- s
+						fmt.Printf("%s", string(buf[:n]))
 					}
 				}
-			}()
+			} else {
+				ips, err := common.GetHostAddrs(true, nil)
+				if common.Error(err) {
+					return err
+				}
 
-			for socket == nil {
-				select {
-				case <-time.After(time.Millisecond * 250):
-					continue
-				case socket = <-socketCh:
-					break
+				common.Info("Local IPs: %v", ips)
+				common.Info("Accept connection: %s...", *server)
+
+				socketCh := make(chan net.Conn)
+				socket = nil
+
+				go func() {
+
+					if *useTls {
+						s, err := listener.Accept()
+						common.Error(err)
+
+						tlsConn, ok := s.(*tls.Conn)
+						if ok {
+							err := tlsConn.Handshake()
+
+							if common.Error(err) {
+								common.Error(s.Close())
+							}
+						}
+
+						if s != nil {
+							socketCh <- s
+						}
+					} else {
+						s, err := tcpListener.AcceptTCP()
+						common.Error(err)
+
+						if s != nil {
+							socketCh <- s
+						}
+					}
+				}()
+
+				for socket == nil {
+					select {
+					case <-time.After(time.Millisecond * 250):
+						continue
+					case socket = <-socketCh:
+						break
+					}
 				}
 			}
 		} else {
@@ -196,9 +317,9 @@ func run() error {
 			} else {
 				common.Info("Timeout: %v", common.MillisecondToDuration(*loopTimeout))
 
-				if *useTls && *loopCount > 1 {
+				if (*useTls || isSerialPortOptions(*client)) && *loopCount > 1 {
 					*loopCount = 1
-					common.Info("As of TLS connection loop count is reset to 1")
+					common.Info("Loop count forced to be reset to 1")
 				} else {
 					common.Info("Loop count: %d", *loopCount)
 				}
@@ -210,72 +331,88 @@ func run() error {
 				}
 			}
 
-			if *useTls {
-				tlsPackage, err := common.GetTlsPackage()
+			if isSerialPortOptions(*client) {
+				spo, err := evaluateSerialPortOptions(*client)
 				if common.Error(err) {
 					return err
 				}
 
-				hostname, _, err := net.SplitHostPort(*client)
+				socket, err = serial.Open(*spo)
 				if common.Error(err) {
 					return err
-				}
-
-				if hostname == "" {
-					hostname = "localhost"
-				}
-
-				// set hostname for self-signed cetificates
-				tlsPackage.Config.ServerName = hostname
-				tlsPackage.Config.InsecureSkipVerify = !*useTlsVerify
-
-				common.Info("Dial TLS connection: %s...", *client)
-
-				socket, err = tls.Dial("tcp", *client, &tlsPackage.Config)
-				if common.Error(err) {
-					return err
-				}
-
-				var ok bool
-
-				tlsSocket, ok = socket.(*tls.Conn)
-				if ok {
-					if *showTlsInfo {
-						ba, err := json.MarshalIndent(tlsSocket.ConnectionState(), "", "    ")
-						if common.Error(err) {
-							return err
-						}
-
-						common.Info("TLS connection state: %s", string(ba))
-					}
-
-					if !tlsSocket.ConnectionState().HandshakeComplete {
-						return fmt.Errorf("TLS handshake not completed")
-					}
 				}
 			} else {
-				common.Info("Dial connection: %s...", *client)
+				if *useTls {
+					tlsPackage, err := common.GetTlsPackage()
+					if common.Error(err) {
+						return err
+					}
 
-				tcpAddr, err := net.ResolveTCPAddr("tcp", *client)
-				if common.Error(err) {
-					return err
+					hostname, _, err := net.SplitHostPort(*client)
+					if common.Error(err) {
+						return err
+					}
+
+					if hostname == "" {
+						hostname = "localhost"
+					}
+
+					// set hostname for self-signed cetificates
+					tlsPackage.Config.ServerName = hostname
+					tlsPackage.Config.InsecureSkipVerify = !*useTlsVerify
+
+					common.Info("Dial TLS connection: %s...", *client)
+
+					socket, err = tls.Dial("tcp", *client, &tlsPackage.Config)
+					if common.Error(err) {
+						return err
+					}
+
+					var ok bool
+
+					tlsSocket, ok = socket.(*tls.Conn)
+					if ok {
+						if *showTlsInfo {
+							ba, err := json.MarshalIndent(tlsSocket.ConnectionState(), "", "    ")
+							if common.Error(err) {
+								return err
+							}
+
+							common.Info("TLS connection state: %s", string(ba))
+						}
+
+						if !tlsSocket.ConnectionState().HandshakeComplete {
+							return fmt.Errorf("TLS handshake not completed")
+						}
+					}
+				} else {
+					common.Info("Dial connection: %s...", *client)
+
+					tcpAddr, err := net.ResolveTCPAddr("tcp", *client)
+					if common.Error(err) {
+						return err
+					}
+
+					tcpCon, err := net.DialTCP("tcp", nil, tcpAddr)
+					if common.Error(err) {
+						return err
+					}
+
+					socket = tcpCon
 				}
-
-				tcpCon, err := net.DialTCP("tcp", nil, tcpAddr)
-				if common.Error(err) {
-					return err
-				}
-
-				socket = tcpCon
 			}
 		}
 
-		common.Info("Connected: %s", socket.RemoteAddr().String())
+		if asSocket(socket) != nil {
+			common.Info("Connected: %s", asSocket(socket).RemoteAddr().String())
+		} else {
+			common.Info("Connected: %s", *client)
+		}
 
 		ba := make([]byte, blockSize)
 
 		if *server != "" {
-			go func(socket net.Conn) {
+			go func(socket io.ReadWriteCloser) {
 				hasher := startSession()
 
 				var f io.Writer
@@ -388,9 +525,12 @@ func run() error {
 
 				for i := 0; i < *loopCount; i++ {
 					deadline := time.Now().Add(common.MillisecondToDuration(*loopTimeout))
-					err = socket.SetDeadline(deadline)
-					if err != nil {
-						return err
+
+					if asSocket(socket) != nil {
+						err = asSocket(socket).SetDeadline(deadline)
+						if err != nil {
+							return err
+						}
 					}
 
 					n, err = io.CopyBuffer(writer, reader, ba)
@@ -404,10 +544,10 @@ func run() error {
 					if *loopCount > 1 {
 						common.Info("Loop #%d Bytes sent: %s/%v", i, common.FormatMemory(int(n)), common.MillisecondToDuration(*loopTimeout))
 
-						if *interTimeout > 0 {
-							common.Info("intermediate sleep timeout: %v", common.MillisecondToDuration(*interTimeout))
+						if *loopSleep > 0 {
+							common.Info("intermediate sleep timeout: %v", common.MillisecondToDuration(*loopSleep))
 
-							time.Sleep(common.MillisecondToDuration(*interTimeout))
+							time.Sleep(common.MillisecondToDuration(*loopSleep))
 						}
 					} else {
 						common.Info("Bytes sent: %s/%v", common.FormatMemory(int(n)), common.MillisecondToDuration(*loopTimeout))
