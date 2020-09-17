04e1c14ed23778ea6200ac01fa547004bb57059e
mpetavy <mpetavy>
2020-09-17 23:58:22 +0200

    some missed callback.ignore -> callback.noerrors

--- build-lxc.sh
+++ build-lxc.sh
@@ -3,7 +3,7 @@ tar -zxvf netio-1.0.0-1234-linux-amd64.tar.gz -C /tmp ./netio
 lxc delete netio --force
 lxc launch images:debian/10 netio
 lxc file push /tmp/netio netio/root/
-lxc exec netio -- /root/netio -log.verbose -log.file=/root/netio.log -cfg.file=/root/netio.json -app.product=edgebox -callback.ignore -service=install
+lxc exec netio -- /root/netio -log.verbose -log.file=/root/netio.log -cfg.file=/root/netio.json -app.product=edgebox -callback.noerrors -service=install
 lxc exec netio -- systemctl enable netio.service
 lxc exec netio -- systemctl start netio.service
 # lxc exec netio -- /bin/bash
