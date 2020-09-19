39b5b5f833d8991a82e7399a7114d3f1deee0fb6
mpetavy <mpetavy>
2020-09-20 01:27:41 +0200

    TimeoutReader, DeadlineReader, netio refactored, endpoint.go

---
+++ endpoint/endpoint.go
@@ -0,0 +1,13 @@
+package endpoint
+
+import "io"
+
+type Endpoint interface {
+	Start() error
+	Stop() error
+	GetConnection() (Connection, error)
+}
+
+type Connection interface {
+	io.ReadWriteCloser
+}
