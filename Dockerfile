29e2f8d58278cd1a77da142d1c8b4c1f1faa22bb
mpetavy <mpetavy>
2021-05-14 11:33:12 +0200

    Dockerfiles adjusted

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ ARG LDFLAG_LICENSE
 ARG LDFLAG_VERSION
 ARG LDFLAG_EXPIRE
 ARG LDFLAG_GIT
-ARG LDFLAG_COUNTER
+ARG LDFLAG_BUILD
 
 RUN echo "LDFLAG_DEVELOPER: $LDFLAG_DEVELOPER" > /tmp/report \
     && echo "LDFLAG_HOMEPAGE: $LDFLAG_HOMEPAGE" >> /tmp/report \
@@ -18,7 +18,7 @@ RUN echo "LDFLAG_DEVELOPER: $LDFLAG_DEVELOPER" > /tmp/report \
     && echo "LDFLAG_VERSION: $LDFLAG_VERSION" >> /tmp/report \
     && echo "LDFLAG_EXPIRE: $LDFLAG_EXPIRE" >> /tmp/report \
     && echo "LDFLAG_GIT: $LDFLAG_GIT" >> /tmp/report \
-    && echo "LDFLAG_COUNTER: $LDFLAG_COUNTER" >> /tmp/report \
+    && echo "LDFLAG_BUILD: $LDFLAG_BUILD" >> /tmp/report \
     && cat /tmp/report \
     && mkdir -p /var/lib/dbus \
     && echo "machine-id" > /var/lib/dbus/machine-id
@@ -43,7 +43,7 @@ RUN mkdir dist-windows-amd64 \
     && export CGO_ENABLED=0 \
     && export GOOS=windows \
     && export GOARCH=amd64 \
-    && go build -mod=vendor -ldflags="-s -w -X main.LDFLAG_DEVELOPER=$LDFLAG_DEVELOPER -X main.LDFLAG_HOMEPAGE=$LDFLAG_HOMEPAGE -X main.LDFLAG_LICENSE=$LDFLAG_LICENSE -X main.LDFLAG_VERSION=$LDFLAG_VERSION -X main.LDFLAG_EXPIRE=$LDFLAG_EXPIRE -X main.LDFLAG_GIT=$LDFLAG_GIT -X main.LDFLAG_COUNTER=$LDFLAG_COUNTER" -o dist-windows-amd64/netio.exe .
+    && go build -mod=vendor -ldflags="-s -w -X main.LDFLAG_DEVELOPER=$LDFLAG_DEVELOPER -X main.LDFLAG_HOMEPAGE=$LDFLAG_HOMEPAGE -X main.LDFLAG_LICENSE=$LDFLAG_LICENSE -X main.LDFLAG_VERSION=$LDFLAG_VERSION -X main.LDFLAG_EXPIRE=$LDFLAG_EXPIRE -X main.LDFLAG_GIT=$LDFLAG_GIT -X main.LDFLAG_BUILD=$LDFLAG_BUILD" -o dist-windows-amd64/netio.exe .
 
 # build Linux-amd64, also with Docker support
 
@@ -59,7 +59,7 @@ RUN mkdir dist-linux-amd64 \
     && export CGO_ENABLED=0 \
     && export GOOS=linux \
     && export GOARCH=amd64 \
-    && go build -mod=vendor -ldflags="-s -w -X main.LDFLAG_DEVELOPER=$LDFLAG_DEVELOPER -X main.LDFLAG_HOMEPAGE=$LDFLAG_HOMEPAGE -X main.LDFLAG_LICENSE=$LDFLAG_LICENSE -X main.LDFLAG_VERSION=$LDFLAG_VERSION -X main.LDFLAG_EXPIRE=$LDFLAG_EXPIRE -X main.LDFLAG_GIT=$LDFLAG_GIT -X main.LDFLAG_COUNTER=$LDFLAG_COUNTER" -o dist-linux-amd64/netio .
+    && go build -mod=vendor -ldflags="-s -w -X main.LDFLAG_DEVELOPER=$LDFLAG_DEVELOPER -X main.LDFLAG_HOMEPAGE=$LDFLAG_HOMEPAGE -X main.LDFLAG_LICENSE=$LDFLAG_LICENSE -X main.LDFLAG_VERSION=$LDFLAG_VERSION -X main.LDFLAG_EXPIRE=$LDFLAG_EXPIRE -X main.LDFLAG_GIT=$LDFLAG_GIT -X main.LDFLAG_BUILD=$LDFLAG_BUILD" -o dist-linux-amd64/netio .
 
 # build and tar for Linux-arm6
 
@@ -69,20 +69,13 @@ RUN mkdir dist-linux-arm6 \
     && export CGO_ENABLED=0 \
     && export GOOS=linux \
     && export GOARCH=arm \
-    && go build -mod=vendor -ldflags="-s -w -X main.LDFLAG_DEVELOPER=$LDFLAG_DEVELOPER -X main.LDFLAG_HOMEPAGE=$LDFLAG_HOMEPAGE -X main.LDFLAG_LICENSE=$LDFLAG_LICENSE -X main.LDFLAG_VERSION=$LDFLAG_VERSION -X main.LDFLAG_EXPIRE=$LDFLAG_EXPIRE -X main.LDFLAG_GIT=$LDFLAG_GIT -X main.LDFLAG_COUNTER=$LDFLAG_COUNTER" -o dist-linux-arm6/netio .
-
-# run  tests
-
-RUN rm -rf /tmp/dist-linux-amd64 \
-    && cp -r dist-linux-amd64 /tmp/
-
-WORKDIR /go/src/netio
+    && go build -mod=vendor -ldflags="-s -w -X main.LDFLAG_DEVELOPER=$LDFLAG_DEVELOPER -X main.LDFLAG_HOMEPAGE=$LDFLAG_HOMEPAGE -X main.LDFLAG_LICENSE=$LDFLAG_LICENSE -X main.LDFLAG_VERSION=$LDFLAG_VERSION -X main.LDFLAG_EXPIRE=$LDFLAG_EXPIRE -X main.LDFLAG_GIT=$LDFLAG_GIT -X main.LDFLAG_BUILD=$LDFLAG_BUILD" -o dist-linux-arm6/netio .
 
 # tar all builds along with netio-tests.html test result file
 
-RUN tar -czvf netio-$LDFLAG_VERSION-$LDFLAG_COUNTER-windows-amd64.tar.gz -C dist-windows-amd64/ . \
-    && tar -czvf netio-$LDFLAG_VERSION-$LDFLAG_COUNTER-linux-amd64.tar.gz -C dist-linux-amd64/ . \
-    && tar -czvf netio-$LDFLAG_VERSION-$LDFLAG_COUNTER-linux-arm6.tar.gz -C dist-linux-arm6/ .
+RUN tar -czvf netio-$LDFLAG_VERSION-$LDFLAG_BUILD-windows-amd64.tar.gz -C dist-windows-amd64/ . \
+    && tar -czvf netio-$LDFLAG_VERSION-$LDFLAG_BUILD-linux-amd64.tar.gz -C dist-linux-amd64/ . \
+    && tar -czvf netio-$LDFLAG_VERSION-$LDFLAG_BUILD-linux-arm6.tar.gz -C dist-linux-arm6/ .
 
 # report artifacts
 
--- build.sh
+++ build.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 
-set -e
-set -x
+# to build this image: ./build.sh
+# to run this image: docker run --env netio_limit_netio_port=9999 --name netio-app netio-app
+
+set -e # stop on error in script
+set -x # log script steps to STDOUT
 
 export APP="netio"
-export SVC_CMD="-log.verbose -log.file=/root/$APP.log -cfg.file=/root/$APP.json -service=install -s :15000"
+export SVC_CMD="-log.verbose -log.file=/root/$APP.log -cfg.file=/root/$APP.json -service=install"
 
 git rev-parse HEAD >/tmp/GIT_TAG.txt
 read GIT_TAG </tmp/GIT_TAG.txt
 
 if [ -z "$GO_VERSION" ]; then
-  export GO_VERSION="1.16.3"
+  export GO_VERSION="1.16.4"
 fi
 
 if [ -z "$TEAMCITY_VERSION" ]; then
   export LDFLAG_DEVELOPER=mpetavy
   export LDFLAG_HOMEPAGE=https://github.com/mpetavy/$APP
   export LDFLAG_LICENSE=https://www.apache.org/licenses/LICENSE-2.0.html
-  export LDFLAG_VERSION=1.0.3
+  export LDFLAG_VERSION=1.0.4
   export LDFLAG_EXPIRE=
-  export LDFLAG_GIT=$GIT_TAG
-  export LDFLAG_BUILD=1
+  export LDFLAG_GIT=$(git rev-parse HEAD)
+  export LDFLAG_BUILD="1234"
 
-  DOCKER_IMAGE=golang:$GO_VERSION
+  export DOCKER_IMAGE=golang:$GO_VERSION
 else
-  DOCKER_IMAGE=artifactory-medmuc:8084/docker-hub/golang:$GO_VERSION
+  export DOCKER_IMAGE=artifactory-medmuc:8084/docker-hub/golang:$GO_VERSION
 fi
 
 [ -e $APP-$LDFLAG_VERSION-$LDFLAG_BUILD-windows-amd64.tar.gz ] && rm $APP-$LDFLAG_VERSION-$LDFLAG_BUILD-windows-amd64.tar.gz
@@ -41,7 +44,7 @@ fi
 
 git log --pretty=format:"%h | %ai | %s %d [%an]" >CHANGES.txt
 
-docker image build --rm -t $APP --target builder --build-arg DOCKER_IMAGE="$DOCKER_IMAGE" --build-arg LDFLAG_DEVELOPER="$LDFLAG_DEVELOPER" --build-arg LDFLAG_HOMEPAGE="$LDFLAG_HOMEPAGE" --build-arg LDFLAG_LICENSE="$LDFLAG_LICENSE" --build-arg LDFLAG_VERSION="$LDFLAG_VERSION" --build-arg LDFLAG_EXPIRE="$LDFLAG_EXPIRE" --build-arg LDFLAG_GIT="$LDFLAG_GIT" --build-arg LDFLAG_BUILD="$LDFLAG_BUILD" .
+docker image build --force-rm --rm -t $APP --target builder --build-arg DOCKER_IMAGE="$DOCKER_IMAGE" --build-arg LDFLAG_DEVELOPER="$LDFLAG_DEVELOPER" --build-arg LDFLAG_HOMEPAGE="$LDFLAG_HOMEPAGE" --build-arg LDFLAG_LICENSE="$LDFLAG_LICENSE" --build-arg LDFLAG_VERSION="$LDFLAG_VERSION" --build-arg LDFLAG_EXPIRE="$LDFLAG_EXPIRE" --build-arg LDFLAG_GIT="$LDFLAG_GIT" --build-arg LDFLAG_BUILD="$LDFLAG_BUILD" .
 
 rm CHANGES.txt
 
@@ -51,44 +54,67 @@ docker cp $APP:/go/src/$APP/$APP-$LDFLAG_VERSION-$LDFLAG_BUILD-windows-amd64.tar
 docker cp $APP:/go/src/$APP/$APP-$LDFLAG_VERSION-$LDFLAG_BUILD-linux-amd64.tar.gz .
 docker cp $APP:/go/src/$APP/$APP-$LDFLAG_VERSION-$LDFLAG_BUILD-linux-arm6.tar.gz .
 
+if [ -z "$TEAMCITY_VERSION" ]; then
+  if [ ! -z "$(docker images -q $APP-app)" ]; then
+    docker image rm -f $APP-app
+  fi
+
+  if [ ! -z "$(docker ps -a | grep -i $APP-app)" ]; then
+    docker container rm -f $APP-app
+  fi
+
+  docker cp $APP:/go/src/$APP/dist-linux-amd64/netio .
+
+  docker image build --force-rm -t $APP-app -f ./Dockerfile-run .
+
+  rm ./netio
+
+  # docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  # docker tag netio-app mpetavy/netio-app
+  # docker commit -m "netio-app" -a mpetavy `docker container ls -lq` mpetavy/netio-app
+  # docker push mpetavy/netio-app
+fi
+
 if [ ! -z "$TEAMCITY_VERSION" ]; then
   curl --noproxy "*" -u "$ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD" -H "Content-Type: application/octet-stream" -X PUT "http://artifactory-medmuc:8084/artifactory/$APP-release-local/$LDFLAG_VERSION/$APP-$LDFLAG_VERSION-$LDFLAG_BUILD-windows-amd64.tar.gz" -T $APP-$LDFLAG_VERSION-$LDFLAG_BUILD-windows-amd64.tar.gz
   curl --noproxy "*" -u "$ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD" -H "Content-Type: application/octet-stream" -X PUT "http://artifactory-medmuc:8084/artifactory/$APP-release-local/$LDFLAG_VERSION/$APP-$LDFLAG_VERSION-$LDFLAG_BUILD-linux-amd64.tar.gz" -T $APP-$LDFLAG_VERSION-$LDFLAG_BUILD-linux-amd64.tar.gz
   curl --noproxy "*" -u "$ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD" -H "Content-Type: application/octet-stream" -X PUT "http://artifactory-medmuc:8084/artifactory/$APP-release-local/$LDFLAG_VERSION/$APP-$LDFLAG_VERSION-$LDFLAG_BUILD-linux-arm6.tar.gz" -T $APP-$LDFLAG_VERSION-$LDFLAG_BUILD-linux-arm6.tar.gz
 fi
 
-if [ -e "$1" ] || [ "$OS" == "Windows_NT" ]; then
+if [ "#$OS#" = '#Windows_NT#' -o "#$BUILD_LXC#" = '##' -o "#$(which lxc)#" = '##' ]; then
   echo WARNING! No LXC container is created!
-else
-  docker cp $APP:/go/src/$APP/dist-linux-amd64/$APP /tmp
 
-  if [ ! -z "$(lxc list | grep -i $APP)" ]; then
-    lxc delete $APP --force
-  fi
-
-  lxc launch images:debian/10 $APP
-  lxc file push /tmp/$APP $APP/root/
-  lxc exec $APP -- /root/$APP $SVC_CMD
-  lxc exec $APP -- systemctl enable $APP.service
-  lxc exec $APP -- systemctl start $APP.service
-
-  # LXD/LXC Version 3
-
-  lxc snapshot $APP snapshot
-  lxc publish $APP/snapshot --alias $APP-snapshot
-  lxc image export $APP-snapshot .
-  lxc image delete $APP-snapshot
-
-  mv $(ls -t | head -n1) $APP-$LDFLAG_VERSION-$LDFLAG_BUILD-lxc.tar.gz
-
-  # LXD/LXC Version 4
-
-  # lxc export $APP $APP-$LDFLAG_VERSION-$LDFLAG_BUILD-lxc.tar.gz --instance-only
-
-  if [ ! -z "$TEAMCITY_VERSION" ]; then
-    curl --noproxy "*" -u "$ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD" -H "Content-Type: application/octet-stream" -X PUT "http://artifactory-medmuc:8084/artifactory/$APP-release-local/$LDFLAG_VERSION/$APP-$LDFLAG_VERSION-$LDFLAG_BUILD-lxc.tar.gz" -T $APP-$LDFLAG_VERSION-$LDFLAG_BUILD-lxc.tar.gz
-  fi
-
-  docker container rm -f $APP
-  docker image rm -f $APP
+  exit 0
 fi
+
+docker cp $APP:/go/src/$APP/dist-linux-amd64/$APP /tmp
+
+if [ ! -z "$(lxc list | grep -i $APP)" ]; then
+  lxc delete $APP --force
+fi
+
+lxc launch images:debian/10 $APP
+lxc file push /tmp/$APP $APP/root/
+lxc exec $APP -- /root/$APP $SVC_CMD
+lxc exec $APP -- systemctl enable $APP.service
+lxc exec $APP -- systemctl start $APP.service
+
+# LXD/LXC Version 3
+
+lxc snapshot $APP snapshot
+lxc publish $APP/snapshot --alias $APP-snapshot
+lxc image export $APP-snapshot .
+lxc image delete $APP-snapshot
+
+mv $(ls -t | head -n1) $APP-$LDFLAG_VERSION-$LDFLAG_BUILD-lxc.tar.gz
+
+# LXD/LXC Version 4
+
+# lxc export $APP $APP-$LDFLAG_VERSION-$LDFLAG_BUILD-lxc.tar.gz --instance-only
+
+if [ ! -z "$TEAMCITY_VERSION" ]; then
+  curl --noproxy "*" -u "$ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD" -H "Content-Type: application/octet-stream" -X PUT "http://artifactory-medmuc:8084/artifactory/$APP-release-local/$LDFLAG_VERSION/$APP-$LDFLAG_VERSION-$LDFLAG_BUILD-lxc.tar.gz" -T $APP-$LDFLAG_VERSION-$LDFLAG_BUILD-lxc.tar.gz
+fi
+
+docker container rm -f $APP
+docker image rm -f $APP
